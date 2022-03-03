ece8c6ddf6317b723ac5091c92b14464b1a6441a
yaakovab <jia17511@gmail.com>
2022-03-03 15:40:16 +0200

    deploying app

---
+++ index.js
@@ -0,0 +1,89 @@
+const express = require('express')
+const app = express()
+const cors = require('cors')
+
+app.use(express.json())
+app.use(cors())
+
+let notes = [
+    {
+        id: 1,
+        content: "HTML is easy",
+        date: "2022-05-30T17:30:31.098Z",
+        important: true
+    },
+    {
+        id: 2,
+        content: "Browser can execute only Javascript",
+        date: "2022-05-30T18:39:34.091Z",
+        important: false
+    },
+    {
+        id: 3,
+        content: "GET and POST are the most important methods of HTTP protocol",
+        date: "2022-05-30T19:20:14.298Z",
+        important: true
+    }
+]
+
+app.get('/', (request, response) => {
+    response.send('<h1>hello World!!!</h1>')
+})
+
+app.get('/api/notes', (request, response) => {
+    response.json(notes)
+})
+
+app.get('/api/notes/:id', (request, response) => {
+    const id = Number(request.params.id)
+    const note = notes.find(note => note.id === id)
+
+    if (note) {
+        response.json(note)
+    }
+    else {
+        response.status(404).end()
+    }
+})
+
+app.delete('/api/notes/:id', (request, response) => {
+    const id = Number(request.params.id)
+    notes = notes.filter(note => note.id !== id)
+
+    response.status(204).end()
+})
+
+app.post('/api/notes', (req, res) => {
+    const body = req.body
+
+    if (!body.content) {
+        return res.status(400).json({ error: 'cntent is missing' })
+    }
+
+    const note = {
+        content: body.content,
+        important: body.important || false,
+        date: new Date(),
+        id: generateID()
+    }
+
+    notes = notes.concat(note)
+    console.log(note)
+    res.json(note)
+})
+
+const unknownEndpoint = (req, res) => {
+    res.status(404).send({ error: 'unknown endpoint' })
+}
+
+app.use(unknownEndpoint)
+
+const PORT = process.env.PORT || 3001
+app.listen(PORT)
+console.log(`Server runing on port ${PORT}`)
+
+function generateID() {
+    return notes.length > 0
+        ? Math.max(...notes.map(n => n.id))
+        : 0
+}
