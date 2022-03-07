dd2a6d42933c4877eaba387cfcf02e127b886e20
yaakovab <jia17511@gmail.com>
2022-03-07 13:00:29 +0200

    production to repo

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const cors = require('cors')
 
 app.use(express.json())
 app.use(cors())
+app.use(express.static('build'))
 
 let notes = [
     {
@@ -26,9 +27,9 @@ let notes = [
     }
 ]
 
-app.get('/', (request, response) => {
-    response.send('<h1>hello World!!!</h1>')
-})
+// app.get('/', (request, response) => {
+//     response.send('<h1>hello World!!!</h1>')
+// })
 
 app.get('/api/notes', (request, response) => {
     response.json(notes)
